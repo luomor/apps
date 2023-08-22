626733abf0e509217cd4efa12f3be920f6a31819
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-08-22 13:12:13 +0800

    update

--- public/qixi2023/scripts/main.js
+++ public/qixi2023/scripts/main.js
@@ -224,7 +224,7 @@
   
   var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
     if(!isChrome){
-      $('#iframeAudio').remove()
+      
   }
   
   // setTimeout(() => {
